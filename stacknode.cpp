ab33f530c179f2572bf204d4347679519f293587
Bonghee Hong <bhhong@pusan.ac.kr>
2019-10-30 20:57:23 +0900

    19.10.30

--- stacknode.cpp
+++ stacknode.cpp
@@ -1,100 +1,16 @@
-//Stack node & Queue Node
-#include <stdio.h>
 #include <iostream>
+#include "stacknode.h"
 using namespace std;
-template <class T>
-class Stack; //forward declaration
-
-template <class T>
-class StackNode {
-	friend class Stack<T>;
-private:
-	T data;
-	StackNode<T>* link;
-	StackNode<T>(T d = 0, StackNode<T>* l = 0) : data(d), link(l) {};//constructor
-};
-/*
-template <class T>
-class QueueNode {
-	friend class Queue<T>;
-private:
-	T data;
-	QueueNode<T>* front;
-	QueueNode<T>* rear;
-	QueueNode<T>(T d = 0, QueueNode<T>*l = 0) : data(d), link(l) {};//constructor
-};
-*/
-template <class T>
-class Stack {
-public:
-	Stack() { top = 0; };//constructor
-	void Add(const T);
-	T* Delete(T&);
-private:
-	StackNode<T>* top;
-	void StackEmpty();
-
-};
-/*
-template <class T>
-class Queue {
-public:
-	Queue() { front = rear = 0; };//constructor
-	void Add(const T);
-	T* Delete(T&);
-private:
-	QueueNode<T>* top;
-	void QueueEmpty();
-
-};
-*/
-template <class T>
-void Stack<T>::Add(const int y) {
-	top = new StackNode<T>(y, top);
-}
-
-template <class T>
-T* Stack<T>::Delete(T& retvalue)
-//delete the top node from stack and return a pointer to its data
-{
-	if (top == ) {
-		StackEmpty(); return 0;
-	} //retrun null pointer constant
-	StackNode* x = top;
-	retvalue = top->data; //get data field of top node
-	top = x->link;//remove top node
-	delete x;
-	return &retvalue;//return pointer to data
-}
-/*
-template <class T>
-void Queue<T>::Add(const T y) {
-	if (front == 0) front = rear = new QueueNode(y,0);//empty queue
-	else rear = rear->link = new QueueNode(y,0); //attach node and update rear
-}
-
-template <class T>
-T* Queue<T>::Delete(T& retvalue)
-//delete the first node in queue and return a pointer to its data
-{
-	if (front == 0) {QueueEmpty(); return 0;}
-	QueueNode *x = front;
-	retvalue = front->data; //get data field of front node
-	front = x->link;//remove front node
-	delete x;
-	return &retvalue;//return pointer to data
-}
-*/
 int main(void) {
 
 	char select;
-	StatckNode<int> sn;
+	StackNode<int> sn;
 	Stack<int> a;
-	/*
+	
 	QueueNode<int> qn;
 	Queue<int> q;
-	*/
-	int value;
+	
+	int value, result;
 
 	cout << endl << "Select command:A: Add to Stack, D: Delete from Stack, P: PrintAll";
 	cin >> select;
@@ -108,7 +24,7 @@ int main(void) {
 			a.Add(value);
 			break;
 		case 'D':
-			a.Delete();
+			result = *a.Delete(value);
 			break;
 		case 'P': //print all elements 
 			for (;;) // coding Ãß°¡
